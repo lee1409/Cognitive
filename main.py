99aee4491eb550a043542fcab817a0ba1ed3ab9a
lee <leerun1409@gmail.com>
2019-05-04 06:57:05 +0000

    Update

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def login_page():
     default_name = '0'
     data = ast.literal_eval(request.form.get('data', default_name))
     correct = 0
-    for i in data[1]:
+    for i in data["1"]:
         if "true" in i:
             correct += 1
 
     with open('data.csv', 'a') as csvFile:
         writer = csv.writer(csvFile)
-        writer.writerow(data[1])
+        writer.writerow(data["1"])
     return render_template('submit.html', correct=correct)
 
 
