9ad1165d82aaa69a480be325b474c2dbd5718800
lee1409 <leerun1409@gmail.com>
2019-05-04 14:52:47 +0800

    Haii

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import ast
 from flask import Flask, abort, Response, render_template
 from flask import request
 import os
+import csv
 
 app = Flask(__name__, template_folder=str(os.getcwd()))
 
@@ -11,11 +12,20 @@ app = Flask(__name__, template_folder=str(os.getcwd()))
 def hello():
     return render_template("index.html")
 
+
 @app.route('/submit', methods=["GET", "POST"])
 def login_page():
     default_name = '0'
     data = ast.literal_eval(request.form.get('data', default_name))
-    return str(data["1"])
+    correct = 0
+    for i in data[1]:
+        if "true" in i:
+            correct += 1
+
+    with open('data.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(data[1])
+    return render_template('submit.html', correct=correct)
 
 
 if __name__ == "__main__":
